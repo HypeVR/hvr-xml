15e7677a73d8b25f9cb768b24d1fd28fd1cbee04
caoyangjiang <caoyang@hypevr.com>
2019-06-07 13:21:53 -0700

    - improve pipeline

--- AddPPA.sh
+++ AddPPA.sh
@@ -5,3 +5,7 @@ sudo sh -c "echo Package: \* >> /etc/apt/preferences"
 sudo sh -c "echo Pin: origin ppa.hypevr.com  >> /etc/apt/preferences"
 sudo sh -c "echo Pin-Priority: 1001 >> /etc/apt/preferences"
 sudo apt-get update
+
+# for boost
+sudo add-apt-repository ppa:mhier/libboost-latest
+sudo apt-get update
