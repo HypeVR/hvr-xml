63897afaac965ded44798ccd9dc605aee70866cf
caoyangjiang <caoyang@hypevr.com>
2019-06-07 12:32:36 -0700

    - improve pipeline

--- AddPPA.sh
+++ AddPPA.sh
@@ -4,4 +4,4 @@ sudo sh -c "echo \"deb [arch=amd64 trusted=yes] http://ppa.hypevr.com debian/\" 
 sudo sh -c "echo Package: \* >> /etc/apt/preferences"
 sudo sh -c "echo Pin: origin ppa.hypevr.com  >> /etc/apt/preferences"
 sudo sh -c "echo Pin-Priority: 1001 >> /etc/apt/preferences"
-apt-get update
+sudo apt-get update
